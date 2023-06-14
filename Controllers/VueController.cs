4568dc47b124de06531f16e6cd4b0e04932b6ea7
jhonatan.malta <jhonatan.malta@stand.com.br>
2023-06-14 11:29:16 -0300

    Add project files.

---
+++ Controllers/VueController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetAndVue.Controllers
+{
+    public class VueController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
